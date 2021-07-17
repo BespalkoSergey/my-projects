b455227b0c06f78aabad31846850c5ef74423f06
Sergey Bespalko <sergeyb@rabota.ua>
2021-07-17 17:37:32 +0300

    added simple nodejs server

---
+++ backend/pet-projects/simple-nodejs-server/index.js
@@ -0,0 +1,55 @@
+import * as http from 'http'
+import * as queryString from 'query-string'
+
+const server = http.createServer((req, res) => {
+  switch (req.method) {
+    case 'GET':
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+
+      res.end(`
+            <h1>Form</h1>
+            <form method="post" action="/">
+                <p><input type="text" name="name"/></p>
+                <p><input type="text" name="surname"/></p>
+                <p><input type="email" name="email"/></p>
+                <p><input type="date" name="birthday"/></p>
+                <button type="submit">Send</button>
+            </form>
+            `)
+      break
+    case 'POST':
+      let chunks = []
+
+      req.on('data', (data) => {
+        chunks.push(data)
+      })
+
+      req.on('end', () => {
+        const string = chunks.join('')
+        const post = queryString.parse(string)
+
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+
+        res.end(`
+        <h1>Your personal data:</h1>
+        <p>Name: ${post?.name ?? ''}</p>
+        <p>Surname: ${post?.surname ?? ''}</p>
+        <p>Email: ${post?.email ?? ''}</p>
+        <p>Birthday: ${post?.birthday ?? ''}</p>
+        `)
+      })
+      break
+
+    default:
+      res.writeHead(400, { 'Content-Type': 'text/html' })
+
+      res.end(`
+      <h1>Oops ... Something bad happened</h>
+      `)
+      break
+  }
+})
+
+server.listen(8080, () => {
+  console.log('server is runnig ...')
+})
