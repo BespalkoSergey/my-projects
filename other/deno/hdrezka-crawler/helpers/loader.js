63a47181ccc5d7440e4c386116d0c9c8af019c8f
Sergey Bespalko <sergeyb@rabota.ua>
2021-08-01 15:54:21 +0300

    refacored: loader, performance measurement and added error hanling

--- other/deno/hdrezka-crawler/helpers/loader.js
+++ other/deno/hdrezka-crawler/helpers/loader.js
@@ -3,11 +3,43 @@ export class Loader {
   #chars = ['\\', '|', '/', '-']
   #x = 0
   #intervar = null
+  #encoder
+  #maxValue = 0
+  #currentValue = 0
 
-  constructor() {}
+  constructor() {
+    this.#encoder = new TextEncoder()
+  }
 
-  set text(str) {
-    this.#text = str
+  set maxValue(number) {
+    if (typeof number !== 'number') {
+      console.error('Type Error: Invalid type in Loader as max value of one hundred percent')
+      return
+    }
+
+    this.#maxValue = number
+  }
+
+  set currentValue(number) {
+    if (typeof number !== 'number') {
+      console.error('Type Error: Invalid type in Loader as current value')
+      return
+    }
+
+    this.#currentValue = number
+  }
+
+  get isLoaded() {
+    return this.#getPercents() >= 100
+  }
+
+  #getPercents() {
+    if (this.#currentValue > this.#maxValue) {
+      console.error('Range Error: Invalid range in Loader as current value bigger than max value')
+      return null
+    }
+
+    return Math.round((100 * this.#currentValue) / this.#maxValue) || 0
   }
 
   create() {
@@ -17,12 +49,16 @@ export class Loader {
   }
 
   #log() {
-    const str = '\r' + '    ' + this.#chars[this.#x++] + ' ' + this.#text + '\r'
-    Deno.stdout.write(new TextEncoder().encode(str))
+    const str = `\r    ${this.#chars[this.#x++]} ${this.#getPercents()}%\r`
+    Deno.stdout.write(this.#encoder.encode(str))
     this.#x = this.#x % this.#chars.length
+
+    if (this.isLoaded) {
+      this.#terminate()
+    }
   }
 
-  clear() {
+  #terminate() {
     if (this.#intervar) {
       clearInterval(this.#intervar)
     }
